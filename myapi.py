c987c42165888efcd5ac3e071bdb7337baa133ee
sothylor <49972371+sothylor@users.noreply.github.com>
2023-03-20 16:24:15 +0700

    Update myapi.py

--- myapi.py
+++ myapi.py
@@ -1,6 +1,7 @@
-Token = "6166833642:AAEKBbyDVnbdosa6nbRHuJgzKoKPGKsRqao"
+Token = "Enter your bot token"
 
+# Twilio phone verification
 class TwilioCredentials:
-    account_sid = "ACc6b947a7035e567af23ad1215204e89f"
-    auth_token = "247f284e9f3ba99f9a28a8e6ba8cef2e"
-    verify_sid = "VAa0dc9f4948e364b535fba416cd767395"
+    account_sid = "account_sid"
+    auth_token = "auth_token"
+    verify_sid = "verify_sid"
